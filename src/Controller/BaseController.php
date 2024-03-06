57aadabd22725d301edc5eafe483c31bf9a181c0
root <clement.parisot@epsi.fr>
2024-03-06 09:11:43 +0100

    oopsie api

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -11,23 +11,24 @@ class BaseController extends AbstractController
     #[Route('/', name: 'app_accueil')]
     public function index(): Response
     {
-        return $this->render('base/index.html.twig', [
-        ]);
+        return $this->render('base/index.html.twig', []);
     }
 
     #[Route('/apropos', name: 'app_apropos')]
     public function apropos(): Response
     {
-        return $this->render('base/apropos.html.twig', [
-            
-        ]);
+        return $this->render('base/apropos.html.twig', []);
     }
 
     #[Route('/mentionslegales', name: 'app_mentionslegales')]
     public function mentionslegales(): Response
     {
-        return $this->render('base/mentionslegales.html.twig', [
-            
-        ]);
+        return $this->render('base/mentionslegales.html.twig', []);
+    }
+
+    #[Route('/api', name: 'app_api')]
+    public function api(): Response
+    {
+        return $this->render('base/api.html.twig', []);
     }
 }
